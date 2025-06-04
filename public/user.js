506648c3397f3076454fb35129194e4718a80b6f
kumogumo08 <ebisawat0821dance@eagle.ocn.ne.jp>
2025-06-04 12:33:45 +0900

    保存ボタンが反応しない件の修正

--- public/user.js
+++ public/user.js
@@ -172,7 +172,7 @@ if (!isOwnPage && favoriteBtn) {
 
     if (saveTop) saveTop.addEventListener('click', () => saveProfileAndEventsToServer());
     if (saveBottom) saveBottom.addEventListener('click', () => saveProfileAndEventsToServer());
-    if (savePhotos) savePhotos.addEventListener('click', savePhotosToServer);
+    if (savePhotos) savePhotosBtn.addEventListener('click', savePhotosToServer);
   }
 });
 
